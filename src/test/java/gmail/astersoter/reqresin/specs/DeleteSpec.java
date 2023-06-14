ae9b7c81c8635c330a87bb4461cec3bac633416a
LutSerg <5QGJAZCqesdHUvk>
2023-06-14 23:38:48 +0300

    added steps

--- src/test/java/gmail/astersoter/reqresin/specs/DeleteSpec.java
+++ src/test/java/gmail/astersoter/reqresin/specs/DeleteSpec.java
@@ -5,6 +5,7 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 import static gmail.astersoter.reqresin.helpers.CustomAllureListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
--- src/test/java/gmail/astersoter/reqresin/tests/ReqresInApiTests.java
+++ src/test/java/gmail/astersoter/reqresin/tests/ReqresInApiTests.java
@@ -8,6 +8,7 @@ import gmail.astersoter.reqresin.models.lombok.update.UpdateModelRq;
 import gmail.astersoter.reqresin.models.lombok.update.UpdateModelRs;
 import gmail.astersoter.reqresin.models.lombok.user.UserResponse;
 import gmail.astersoter.reqresin.models.lombok.users.GetUsersModelRs;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static gmail.astersoter.reqresin.specs.CreateUserSpecs.createUserRqSpec;
@@ -22,43 +23,50 @@ import static gmail.astersoter.reqresin.specs.UserSpecs.userRqSpec;
 import static gmail.astersoter.reqresin.specs.UserSpecs.userRsSpec;
 import static gmail.astersoter.reqresin.specs.UsersSpecs.usersRqSpec;
 import static gmail.astersoter.reqresin.specs.UsersSpecs.usersRsSpec;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ReqresInApiTests extends TestBase{
+public class ReqresInApiTests extends TestBase {
 
     @Test
-    void singleUserEmailTest() {
-        UserResponse userResponse = given(userRqSpec)
-                .when()
-                .get("/users/2")
-                .then()
-                .spec(userRsSpec)
-                .extract().as(UserResponse.class);
+    @DisplayName("Тест запроса к Single User")
+    void singleUserTest() {
+        UserResponse userResponse = step("request data", () ->
+                given(userRqSpec)
+                        .when()
+                        .get("/users/2")
+                        .then()
+                        .spec(userRsSpec)
+                        .extract().as(UserResponse.class));
 
+        step("verify response", () -> {
         assertEquals(2, userResponse.getData().getId());
         assertEquals("To keep ReqRes free, contributions towards server costs are appreciated!",
                 userResponse.getSupport().getText());
         assertThat(userResponse.getData().getEmail(), notNullValue());
-
+        });
     }
 
     @Test
+    @DisplayName("Тест на проверку данных юзера в запросе List Users")
     void userInArrayTest() {
 
-        GetUsersModelRs getUsers = given(usersRqSpec)
+        GetUsersModelRs getUsers = step("request data", () ->
+                given(usersRqSpec)
                 .when()
                 .get("/users?page=2")
                 .then()
                 .spec(usersRsSpec)
-                .extract().as(GetUsersModelRs.class);
-
-        assertEquals("Lindsay", getUsers.getData().get(1).getFirstName());
+                .extract().as(GetUsersModelRs.class));
+        step("verify response", () ->
+        assertEquals("Lindsay", getUsers.getData().get(1).getFirstName()));
     }
 
     @Test
+    @DisplayName("Тест на успешную регистрацию")
     void successfulRegisterTest() {
 
         RegisterModelRq rqBody = new RegisterModelRq();
@@ -67,64 +75,76 @@ public class ReqresInApiTests extends TestBase{
         String token = "QpwL5tke4Pnpja7X4";
         int id = 4;
 
-        RegisterModelRs registerResponse = given(registrationRqSpec)
+        RegisterModelRs registerResponse = step("request data", () ->
+                given(registrationRqSpec)
                 .body(rqBody)
                 .when()
                 .post("/register")
                 .then()
                 .spec(registrationRsSpec)
-                .extract().as(RegisterModelRs.class);
+                .extract().as(RegisterModelRs.class));
 
+        step("verify response", () -> {
         assertEquals(token, registerResponse.getToken());
         assertEquals(id, registerResponse.getId());
+        });
     }
 
     @Test
+    @DisplayName("Тест на запрос update user")
     void updateUserTest() {
 
         UpdateModelRq updateRq = new UpdateModelRq();
         updateRq.setName("morpheus");
         updateRq.setJob("zion resident");
 
-        UpdateModelRs updateModelRs = given(updateUserRqSpec)
+        UpdateModelRs updateModelRs = step("request data", () ->
+                given(updateUserRqSpec)
                 .body(updateRq)
                 .when()
                 .put("/users/2")
                 .then()
                 .spec(updateUserRsSpec)
-                .extract().as(UpdateModelRs.class);
-
-        assertThat(updateModelRs.getUpdatedAt(), notNullValue());
+                .extract().as(UpdateModelRs.class));
+        step("verify response", () ->
+        assertThat(updateModelRs.getUpdatedAt(), notNullValue()));
     }
 
     @Test
-    void successfulCreateUserTest () {
+    @DisplayName("Тест на запрос Create User")
+    void successfulCreateUserTest() {
 
         CreateUserModelRq createUserRq = new CreateUserModelRq();
         createUserRq.setName("morpheus");
         createUserRq.setJob("leader");
 
-        CreateUserModelRs createUserRs = given(createUserRqSpec)
+        CreateUserModelRs createUserRs = step("request data", () ->
+                given(createUserRqSpec)
                 .body(createUserRq)
                 .when()
                 .post("users")
                 .then()
                 .spec(createUserRsSpec)
-                .extract().as(CreateUserModelRs.class);
+                .extract().as(CreateUserModelRs.class));
 
-        assertEquals("morpheus", createUserRs.getName());
-        assertThat(createUserRs.getCreatedAt(), notNullValue());
+        step("verify response", () -> {
+            assertEquals("morpheus", createUserRs.getName());
+            assertThat(createUserRs.getCreatedAt(), notNullValue());
+        });
     }
 
 
-
     @Test
+    @DisplayName("Тест на поулчение статуса при запросе Delete")
     void deleteTest() {
+
+        int resultCode = 204;
+        step("test data", () ->
         given(deleteRqSpec)
                 .when()
                 .delete("/users/2")
                 .then()
-                .spec(deleteRsSpec);
+                .spec(deleteRsSpec));
     }
 }
 
